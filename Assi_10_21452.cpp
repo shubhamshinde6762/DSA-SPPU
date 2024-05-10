dd36a554e87d2fd24aa9bae9ae47097da958428b
Shubham Shinde <shubhamshinde6762@gmail.com>
2024-05-10 14:10:24 +0530

    heap all functions added

--- Assi_10_21452.cpp
+++ Assi_10_21452.cpp
@@ -1,71 +1,141 @@
 #include <iostream>
 using namespace std;
+#define MAX 50
 
 class HeapSort
 {
 private:
-    int *heapArray;
-    int heapSize;
+    int *heap;
+    int size;
 
-    void heapify(int arr[], int n, int i)
+    void heapify(int i, int n)
     {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
-        if (left < n && arr[left] > arr[largest])
+        if (left < n && heap[left] > heap[largest])
             largest = left;
 
-        if (right < n && arr[right] > arr[largest])
+        if (right < n && heap[right] > heap[largest])
             largest = right;
 
-        if (largest != i)
+        if (i != largest)
         {
-            swap(arr[i], arr[largest]);
-            heapify(arr, n, largest);
+            swap(heap[i], heap[largest]);
+            heapify(largest, n);
         }
+
+        return;
     }
 
 public:
-    HeapSort(int arr[], int size)
-    {
-        heapArray = arr;
-        heapSize = size;
-    }
+    HeapSort() : heap(new int[MAX]), size(0) {}
 
     void buildHeap()
     {
-        for (int i = heapSize / 2 - 1; i >= 0; i--)
-            heapify(heapArray, heapSize, i);
+        for (int i = (size - 1) / 2; i >= 0; i--)
+            heapify(i, size);
     }
 
     void heapSort()
     {
-        buildHeap();
-        for (int i = heapSize - 1; i > 0; i--)
+        for (int i = size - 1; i > 0; i--)
         {
-            swap(heapArray[0], heapArray[i]);
-            heapify(heapArray, i, 0);
+            swap(heap[0], heap[i]);
+            heapify(0, i);
         }
     }
 
-    void printArray()
+    void printHeap()
     {
-        cout << "Sorted Array: ";
-        for (int i = 0; i < heapSize; i++)
-            cout << heapArray[i] << " ";
+        cout << "Heap: ";
+        for (int i = 0; i < size; i++)
+            cout << heap[i] << " ";
         cout << endl;
+    }
+
+    void insert(int data)
+    {
+        if (size >= MAX)
+        {
+            cout << "Heap is full! Cannot insert more elements.\n";
+            return;
+        }
+
+        int index = size;
+        heap[size++] = data;
+        int parent = (index - 1) / 2;
+
+        while (parent >= 0 && heap[parent] < heap[index])
+        {
+            swap(heap[index], heap[parent]);
+            index = parent;
+            parent = (parent - 1) / 2;
+        }
+
+        cout << "Element " << data << " inserted successfully.\n";
+    }
+
+    void remove()
+    {
+        if (size == 0)
+        {
+            cout << "Heap is empty! Cannot remove any element.\n";
+            return;
+        }
+
+        int data = heap[0];
+        swap(heap[0], heap[size - 1]);
+        size--;
+        heapify(0, size);
+
+        cout << "Element " << data << " removed successfully.\n";
+    }
+
+    int top()
+    {
+        if (this->empty())
+        {
+            cout << "Heap is empty! No top element.\n";
+            return -1;
+        }
+
+        return heap[0];
+    }
+
+    bool empty()
+    {
+        return size == 0;
+    }
+
+    void pop()
+    {
+        if (this->empty())
+        {
+            cout << "Heap is empty! Cannot pop any element.\n";
+            return;
+        }
+
+        remove();
     }
 };
 
 int main()
 {
+    HeapSort heapSort;
     int choice;
+
     do
     {
-        cout << "Menu:\n";
-        cout << "1. Sort an array\n";
-        cout << "2. Exit\n";
+        cout << "\nMenu:\n";
+        cout << "1. Insert an element\n";
+        cout << "2. Remove an element\n";
+        cout << "3. Display the top element\n";
+        cout << "4. Pop the top element\n";
+        cout << "5. Sort the heap\n";
+        cout << "6. Build Heap\n";
+        cout << "7. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
 
@@ -73,29 +143,39 @@ int main()
         {
         case 1:
         {
-            int size;
-            cout << "Enter the size of the array: ";
-            cin >> size;
-
-            int *arr = new int[size];
-            cout << "Enter " << size << " integers: ";
-            for (int i = 0; i < size; i++)
-                cin >> arr[i];
-
-            HeapSort heapSort(arr, size);
-            heapSort.heapSort();
-            heapSort.printArray();
-
-            delete[] arr;
+            int data;
+            cout << "Enter the element to insert: ";
+            cin >> data;
+            heapSort.insert(data);
+            heapSort.printHeap();
             break;
         }
         case 2:
+            heapSort.remove();
+            heapSort.printHeap();
+            break;
+        case 3:
+            cout << "Top element: " << heapSort.top() << endl;
+            break;
+        case 4:
+            heapSort.pop();
+            heapSort.printHeap();
+            break;
+        case 5:
+            heapSort.heapSort();
+            heapSort.printHeap();
+            break;
+        case 6:
+            heapSort.buildHeap();
+            heapSort.printHeap();
+            break;
+        case 7:
             cout << "Exiting...\n";
             break;
         default:
             cout << "Invalid choice! Please try again.\n";
         }
-    } while (choice != 2);
+    } while (choice != 7);
 
     return 0;
 }

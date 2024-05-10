27d25d729b438e0cda049bc0a1308bc033b2d7a5
Shubham Shinde <shubhamshinde6762@gmail.com>
2024-05-10 16:23:35 +0530

    obst added

--- Assi_8_21452.cpp
+++ Assi_8_21452.cpp
@@ -1,176 +1,260 @@
 #include <iostream>
 using namespace std;
 
-class KeyProbabilityPair
+// Define a structure for the nodes of the tree
+struct Node
 {
-public:
-    double key;
-    double probability;
-
-    KeyProbabilityPair() : key(0), probability(0) {}
+    int key;
+    Node *left;
+    Node *right;
+    // Constructor
+    Node(int k)
+    {
+        key = k;
+        left = right = nullptr;
+    }
 };
 
-class OBST
+class obst
 {
-private:
-    double **root_cost;
-    int **root;
-    KeyProbabilityPair *keyProbPairs;
-    int n;
-    const double INF = 1e9;
+    float a[20], b[20], wt[20][20], c[20][20];
+    int r[20][20], n;
 
 public:
-    OBST(int size) : n(size)
+    obst(int x)
     {
-        root_cost = new double *[n];
-        root = new int *[n];
-        keyProbPairs = new KeyProbabilityPair[n];
-        for (int i = 0; i < n; ++i)
+        n = x;
+    }
+    void input_prob();
+    void initialize();
+    void con_obst();
+    void display();
+    void print(int l1, int r1);
+    // Functions for tree creation and traversal
+    Node *construct_optimal_bst(int i, int j);
+    void inorder(Node *root);
+    void preorder(Node *root);
+    void postorder(Node *root);
+};
+
+// obst functions
+
+// Function to take input probabilities
+void obst::input_prob()
+{
+    cout << "Enter the probabilities for successful search:" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "p[" << i << "]" << "=";
+        cin >> a[i];
+    }
+    cout << "Enter the probabilities for unsuccessful search:" << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        cout << "q[" << i << "]" << "=";
+        cin >> b[i];
+    }
+}
+
+// Function to initialize matrices
+void obst::initialize()
+{
+    for (int i = 0; i < n; i++)
+    {
+        c[i][i] = 0.0;
+        r[i][i] = 0;
+        wt[i][i] = b[i];
+        wt[i][i + 1] = b[i] + b[i + 1] + a[i + 1];
+        c[i][i + 1] = b[i] + b[i + 1] + a[i + 1];
+        r[i][i + 1] = i + 1;
+    }
+    c[n][n] = 0.0;
+    r[n][n] = 0;
+    wt[n][n] = b[n];
+}
+
+// Function to construct optimal BST
+void obst::con_obst()
+{
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 0; j <= n - i; j++)
         {
-            root_cost[i] = new double[n];
-            root[i] = new int[n];
-            for (int j = 0; j < n; ++j)
+            wt[j][j + i] = b[j + i] + a[j + i] + wt[j][j + i - 1];
+            c[j][j + i] = 9999; // Initialize to a large value
+            for (int k = j + 1; k <= j + i; k++)
             {
-                root_cost[i][j] = 0;
-                root[i][j] = 0;
-            }
-        }
-    }
-
-    ~OBST()
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            delete[] root_cost[i];
-            delete[] root[i];
-        }
-        delete[] root_cost;
-        delete[] root;
-        delete[] keyProbPairs;
-    }
-
-    void input_keys_and_probabilities()
-    {
-        cout << "Enter the keys and their probabilities:\n";
-        for (int i = 0; i < n; ++i)
-        {
-            cout << "Key " << i + 1 << ": ";
-            cin >> keyProbPairs[i].key;
-            cout << "Probability " << i + 1 << ": ";
-            cin >> keyProbPairs[i].probability;
-        }
-    }
-
-    double sum(const KeyProbabilityPair *keyProbPairs, int start, int end)
-    {
-        double sum = 0;
-        for (int k = start; k <= end; ++k)
-        {
-            sum += keyProbPairs[k].probability;
-        }
-        return sum;
-    }
-
-    void printRootCostMatrix(double **root_cost, int n)
-    {
-        cout << "Root Cost Matrix:" << endl;
-        for (int i = 0; i < n; ++i)
-        {
-            for (int j = 0; j < n; ++j)
-            {
-                cout << root_cost[i][j] << "\t";
-            }
-            cout << endl;
-        }
-    }
-
-    void printRootMatrix(int **root, int n)
-    {
-        cout << "Root Matrix:" << endl;
-        for (int i = 0; i < n; ++i)
-        {
-            for (int j = 0; j < n; ++j)
-            {
-                cout << root[i][j] << "\t";
-            }
-            cout << endl;
-        }
-    }
-
-    double optimal_cost()
-    {
-
-        for (int i = 0; i < n; ++i)
-        {
-            root_cost[i][i] = keyProbPairs[i].probability;
-            root[i][i] = i;
-        }
-
-        for (int l = 2; l <= n; ++l)
-        {
-            for (int i = 0; i <= n - l; ++i)
-            {
-                int j = i + l - 1;
-                root_cost[i][j] = INF;
-                double sum_p = sum(keyProbPairs, i, j);
-
-                for (int k = i; k <= j; ++k)
+                if (c[j][j + i] > (c[j][k - 1] + c[k][j + i]))
                 {
-                    double cost = sum_p + (k > i ? root_cost[i][k - 1] : 0) + (k < j ? root_cost[k + 1][j] : 0);
-                    if (cost < root_cost[i][j])
-                    {
-                        root_cost[i][j] = cost;
-                        root[i][j] = k;
-                    }
+                    c[j][j + i] = c[j][k - 1] + c[k][j + i];
+                    r[j][j + i] = k;
                 }
             }
+            c[j][j + i] += wt[j][j + i];
         }
-
-        printRootCostMatrix(root_cost, n);
-        printRootMatrix(root, n);
-
-        return root_cost[0][n - 1];
     }
-};
+}
+
+// Function to display matrices
+void obst::display()
+{
+    cout << "Weight Matrix" << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            cout << wt[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Cost Matrix" << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Row Matrix" << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            cout << r[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+// Function to print optimal BST
+void obst::print(int l1, int r1)
+{
+    if (l1 >= r1)
+    {
+        return;
+    }
+    if (r[l1][r[l1][r1] - 1] != 0)
+        cout << "\n Left child of " << r[l1][r1] << " :: " << r[l1][r[l1][r1] - 1];
+    if (r[r[l1][r1]][r1] != 0)
+        cout << "\n Right child of " << r[l1][r1] << " :: " << r[r[l1][r1]][r1];
+    print(l1, r[l1][r1] - 1);
+    print(r[l1][r1], r1);
+}
+
+// Function to construct optimal BST recursively
+Node *obst::construct_optimal_bst(int i, int j)
+{
+    if (i > j)
+        return nullptr;
+    int root_index = r[i][j];
+    Node *root = new Node(root_index);
+    root->left = construct_optimal_bst(i, root_index - 1);
+    root->right = construct_optimal_bst(root_index, j);
+    return root;
+}
+
+// Inorder traversal
+void obst::inorder(Node *root)
+{
+    if (root == nullptr)
+        return;
+    inorder(root->left);
+    cout << root->key << " ";
+    inorder(root->right);
+}
+
+// Preorder traversal
+void obst::preorder(Node *root)
+{
+    if (root == nullptr)
+        return;
+    cout << root->key << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Postorder traversal
+void obst::postorder(Node *root)
+{
+    if (root == nullptr)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->key << " ";
+}
 
 int main()
 {
-    int size;
-    cout << "Enter the number of keys: ";
-    cin >> size;
-
-    OBST obst(size);
-
-    int choice;
-    do
+    int x;
+    cout << "Enter number of nodes:";
+    cin >> x;
+    obst o(x);
+    int c = 1;
+    while (c > 0)
     {
-        cout << "\nMenu:\n";
-        cout << "1. Input keys and probabilities\n";
-        cout << "2. Calculate optimal cost\n";
-        cout << "3. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice)
+        cout << "MENU" << endl;
+        cout << "1.Input probabilities" << endl;
+        cout << "2.Initialize" << endl;
+        cout << "3.Obst" << endl;
+        cout << "4.Display" << endl;
+        cout << "5.Print" << endl;
+        cout << "6.Construct Optimal BST" << endl;
+        cout << "7.Inorder Traversal" << endl;
+        cout << "8.Preorder Traversal" << endl;
+        cout << "9.Postorder Traversal" << endl;
+        cout << "0.Exit" << endl;
+        cout << "Enter your choice" << endl;
+        cin >> c;
+        if (c == 1)
         {
-        case 1:
+            o.input_prob();
+        }
+        else if (c == 2)
         {
-            obst.input_keys_and_probabilities();
-            break;
+            o.initialize();
         }
-        case 2:
+        else if (c == 3)
         {
-            double cost = obst.optimal_cost();
-            cout << "Optimal cost: " << cost << endl;
-            break;
+            o.con_obst();
         }
-        case 3:
-            cout << "Exiting...\n";
-            break;
-        default:
-            cout << "Invalid choice. Please enter a valid option.\n";
+        else if (c == 4)
+        {
+            o.display();
         }
-    } while (choice != 3);
-
+        else if (c == 5)
+        {
+            o.print(0, x);
+        }
+        else if (c == 6)
+        {
+            Node *root = o.construct_optimal_bst(0, x);
+            cout << "Optimal BST Constructed!" << endl;
+        }
+        else if (c == 7)
+        {
+            Node *root = o.construct_optimal_bst(0, x);
+            cout << "Inorder Traversal: ";
+            o.inorder(root);
+            cout << endl;
+        }
+        else if (c == 8)
+        {
+            Node *root = o.construct_optimal_bst(0, x);
+            cout << "Preorder Traversal: ";
+            o.preorder(root);
+            cout << endl;
+        }
+        else if (c == 9)
+        {
+            Node *root = o.construct_optimal_bst(0, x);
+            cout << "Postorder Traversal: ";
+            o.postorder(root);
+            cout << endl;
+        }
+    }
     return 0;
 }
